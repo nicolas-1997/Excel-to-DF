204c3771ed0486be190c7012551dce55d8e52cc5
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2022-09-11 15:28:20 -0300

    modificacion de los loggers

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 import logging
 
 formato = ' [%(asctime)s], %(levelname)s, %(message)s, %(filename)s:%(lineno)d'
-logging.basicConfig(filename='loggers.log', level=logging.INFO, format=formato)
+logging.basicConfig(filename='loggers.log', level=logging.INFO, format=formato, encoding='utf-8')
 
 # CONSOLE
 console = logging.StreamHandler()
@@ -25,16 +25,22 @@ logger = logging.getLogger(__name__)
 
 class Read_file:
     def __init__(self, url: str):
+        logger.info('Inicializando la clase Read_File')
         self.url = url
         self.list_sheet_name = self.__get_sheet_from_file('Ref_Hoja_Excel')
         self.unidad = self.__get_sheet_from_file('Nemo_Grupo')
 
     def __get_sheet_from_file(self, column_name):
-        self.read_file = pd.read_excel(
-            self.url, na_values="Missing", sheet_name=0, header=1)
-        self.name_sheet = self.read_file[column_name].unique()
-        return self.name_sheet
-
+        logger.info('Iniciando el metodo get sheet from file')
+        logger.info('Leyendo el Archivo')
+        try:
+            self.read_file = pd.read_excel(
+                self.url, na_values="Missing", sheet_name=0, header=1)
+            self.name_sheet = self.read_file[column_name].unique()
+            return self.name_sheet
+        except IOError as e:
+            logger.error(f'Ocurrio un error: {e}')
+            quit()
     def get_list_sheet_name(self):
         return self.list_sheet_name
 
@@ -63,17 +69,18 @@ class newDF(Read_file):
         - Return:
             - Returns a list with the names of the sheets.
         """
-        logger.info('Intentando cargar la hoja')
+        logger.info('Iniciando el Metodo Read excel file')
         try:
             self.sheetname = self.get_list_sheet_name()
-            logger.info(f'Se cargo correctamente la Hoja: {self.sheetname}')
-            logger.info(f'Leyendo la Hoja: {self.sheetname}')
+            logger.info(f'Leyendo la Hoja: {self.sheetname[0]}')
             self.read_sheet_file = pd.read_excel(
                 self.url, na_values="Missing", sheet_name=self.sheetname[0], header=4)
             logger.info(f'Se leyo correctamente la Hoja: {self.sheetname}')
             return self.read_sheet_file
-        except TypeError as e:
-            logger.error(f'Ocurrio un error: {e}')
+        except:
+            logger.error('Ocurrio un error')
+            quit()
+
 
     def __get_data_from_url(self):
         """
@@ -95,8 +102,9 @@ class newDF(Read_file):
             self.months = self.date_in_url[0][4:6]
             self.years = self.date_in_url[0][:4]
             logger.info('Se obtuvieron exitosamente las fechas.')
-        except TypeError as e:
-            logger.error(f'Ocurrio un error: {e}')
+        except:
+            logger.error('Ocurrio un error')
+            quit()
 
     def get_the_current_quarter(self):
         """
@@ -124,9 +132,11 @@ class newDF(Read_file):
                 self.date, periods=3, freq='MS', normalize=False)
             self.dates = [
                 date for date in self.dates if date <= self.date_file]
+            logger.info('Se obtuvo el trimestre correctamente')
             return self.dates
-        except TypeError as e:
-            logger.error(f'Ocurrio un error: {e}')
+        except:
+            logger.error('Ocurrio un error')
+            quit()
 
     def filter_data_by_quarters(self):
         """
@@ -146,15 +156,16 @@ class newDF(Read_file):
            - |R1       |01|01|01|
            - +------------------+
         """
-
+        logger.info('Filtrando los trimestres')
         self.get_the_current_quarter()
         try:
             self.df_quaters = self.read_sheet_file[[
                 self.read_sheet_file.columns[0], *self.dates]].dropna(subset=[self.read_sheet_file.columns[0]])
+            logger.info('Se filtraron los trimestres correctamente')
             return self.df_quaters
         except:
-            return self.read_sheet_file.dropna(subset=[self.read_sheet_file.columns[0]])
-            print("no tiene las columnas")
+            logger.error('Ocurrio un error')
+            quit()
 
     def create_dataframe(self):
 
@@ -171,6 +182,7 @@ class newDF(Read_file):
             - Returns a final DataFrame, with the columns | YEAR | MONTH | UNIT | R | VALUE | START OF THE MONTH |
         
         """
+        logger.info('Creando el DataFrame')
         self.filter_data_by_quarters()
         self.columnR = self.df_quaters.columns[0]
         self.column_list = [
@@ -185,28 +197,36 @@ class newDF(Read_file):
         self.df.drop(self.df.columns[1], inplace=True, axis=1)
         self.df.rename(columns={self.columnR: "R"}, inplace=True)
         self.df = self.df.reindex(columns=self.columns_df)
+        logger.info('Se creo el DataFrame correctamente')
         return self.df
 
 
     def save_to_csv(self):
+        logger.info('Guardando el dataframe en formato csv')
         self.create_dataframe()
         os.makedirs('csv/', exist_ok=True) 
         self.name = re.sub(r"\s+", "_", self.sheetname[0])
+        logger.info(f'Ubicacion del Archivo: ./csv/{self.name}_{self.years}_{self.months}.csv')
         return self.df.to_csv(f'./csv/{self.name}_{self.years}_{self.months}.csv', index=False, encoding='utf-8', header=self.columns_df)
-
+        
 
 if __name__ == "__main__":
+    #Ubicacion del archivo
     path_file = './ReporteN2-ACHI-Real-202103.xlsx'
     logger.info(f'Leyendo el Archivo: {path_file[2:]}')
+    #Se instancia la clase Read_file
     exc_file = Read_file(path_file)
-    logger.info('Obteniendo la lista de nombre de hojas')
+    logger.info(f'El Archivo: {path_file[2:]} se leyo correctamente')
+    #Pedimos la lista e nombre de hojas al metodo get_list_sheet_name
     num_of_sheet = exc_file.get_list_sheet_name()
-
+    #Se define las columnas para el DataFrame.
     columns = ["AÑO", "MES", "UNIDAD", "R", "VALOR", "INICIO_DEL_MES"]
-
     logger.info('Comenzando a Iterar por cada hoja')
     for sheet in range(len(num_of_sheet)):
         logger.info(f'Sheet {sheet+1}/{len(num_of_sheet)}')
+        #Se crea una instancia del DF
         df = newDF(path_file,
                    num_of_sheet[sheet], columns)
+        logger.info('Se ha creado el DF correctamente')
         df.save_to_csv()
+        logger.info(f'El DF de la hoja {sheet+1}/{len(num_of_sheet)} se guardo correctamente')
